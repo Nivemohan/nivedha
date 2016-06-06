11519b4a425626e6c4f8bccf793807276c282279
Nivemohan <nivemohanraj@gmail.com>
2016-06-06 11:33:10 +0530

    Create sign.c

---
+++ sign.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a>0)
+	printf("it is a positive number");
+	else if(a<0)
+	printf("it is a negative number");
+	else
+	printf("zero");
+}
