5f9129b2e2176bf521d143294ff771aa11163417
Nivemohan <nivemohanraj@gmail.com>
2016-06-16 14:57:57 +0530

    Create even range.c

---
+++ even range.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int l,h,i;
+	scanf("%d %d",&l,&h);
+	printf("the even numbers between %d %d is",l,h);
+	for(i=l;i<=h;i++)
+	{
+		if(i%2==0)
+		printf("%d ",i);
+	}
+	return 0;
+}
