7dda9f39f696975be9706a72f747e784a292bab8
Nivemohan <nivemohanraj@gmail.com>
2016-06-16 15:00:12 +0530

    Update odd range.c

--- odd range.c
+++ odd range.c
@@ -3,7 +3,7 @@
 int main(void) {
 	int l,h,i;
 	scanf("%d %d",&l,&h);
-	printf("the even numbers between %d %d is",l,h);
+	printf("the odd numbers between %d %d is",l,h);
 	for(i=l;i<=h;i++)
 	{
 		if(i%2!=0)
