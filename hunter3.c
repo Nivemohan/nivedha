0b9fb43bf419ec64a74cae5ec71a140588085352
Nivemohan <nivemohanraj@gmail.com>
2016-07-26 23:38:56 +0530

    Create hunter3.c

---
+++ hunter3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[100],i,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(i==a[i])
+		printf("%d ",a[i]);
+	}
+	// your code goes here
+	return 0;
+}
